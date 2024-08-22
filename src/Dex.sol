5d0b9a2fa5925275a3dfce69c1bb166d5ba66c0d
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2024-08-23 01:01:41 +0900

    🛠️ Chore : ready for testing (#1)
    * forge install: forge-std
    * forge install: openzeppelin-contracts

---
+++ src/Dex.sol
@@ -0,0 +1,40 @@
+// IDex.sol
+pragma solidity ^0.8.13;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IDex {
+    function addLiquidity(uint256 amount) external;
+    function removeLiquidity(uint256 amount) external;
+    function swapTokens(
+        address tokenA,
+        address tokenB,
+        uint256 amount
+    ) external;
+}
+
+contract Dex is IDex {
+    IERC20 tokenX;
+    IERC20 tokenY;
+
+    constructor(address tokenA, address tokenB) {
+        tokenX = IERC20(tokenA);
+        tokenY = IERC20(tokenB);
+    }
+
+    function addLiquidity(uint256 amount) public override {
+        // Implementation
+    }
+
+    function removeLiquidity(uint256 amount) public override {
+        // Implementation
+    }
+
+    function swapTokens(
+        address tokenA,
+        address tokenB,
+        uint256 amount
+    ) public override {
+        // Implementation
+    }
+}
